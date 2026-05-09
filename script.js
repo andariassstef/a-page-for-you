71ec280f617074261bdc70220f73ad7c0063ba1e
Stefaye Wadsworth. <andarias614@gmail.com>
2026-05-09 13:52:33 +0700

    Update image and content arrays in script.js

--- script.js
+++ script.js
@@ -1,15 +1,77 @@
 const images = [
-  "https://i.pinimg.com/736x/4b/f5/33/4bf533c598c1dbd6db93d7286213b563.jpg",
+  "albumImage/sasa.jpg",
+  "albumImage/bunga.jpg",
+  "albumImage/album.jpg"
+];
 
-  "https://i.pinimg.com/736x/97/f9/68/97f9682c3bbfbe714b9843fd4fd6bfc0.jpg",
+const contents = [
 
-  "https://i.pinimg.com/736x/6f/c7/75/6fc775f8cc4e6d2d8bb3b4b2b08efc17.jpg"
+  `
+  <p>
+    Thank you for surviving this far, i know it hasn’t always been easy for you.
+    i hope this new age brings you softer days, happier moments, and all the good
+    things you truly deserve. may you always be surrounded by love, kindness,
+    and people who genuinely care about you.
+    <br><br>
+    and please, don’t forget to be kinder to yourself too, okay?
+    you deserve to be loved gently, including by your own self.
+    <br><br>
+    i’m really grateful i got to know you and have you in my life.
+    stay longer, please. i still want to make so many memories with you.
+  </p>
+  `,
+
+  `
+  <div class="gift-box">
+    <p>
+      Maybe flowers can't say everything i feel,
+      but i hope these little things can remind you
+      how special you are to me.
+      <br><br>
+      Thank you for existing,
+      thank you for staying,
+      and thank you for being someone so precious in my life.
+    </p>
+  </div>
+  `,
+
+  `
+  <div class="music-card">
+
+    <img src="albumImage/album.jpg" class="music-cover">
+
+    <h3>Best Part</h3>
+    <p>DAY6</p>
+
+    <p class="music-text">
+      “Best Part” reminds me of the way you make everything feel softer
+      and warmer without even trying.
+      <br><br>
+      thank you for becoming one of the best parts of my days,
+      in ways you probably don’t even realize.
+      <br><br>
+      so whenever you listen to this song,
+      i hope you remember that someone out there
+      loves you very deeply and sincerely.
+    </p>
+
+    <a href="https://open.spotify.com/track/5gBMDX31s0uvslfUtMLHKV?si=RWYDzfHYQniuXmzfqh7mjw"
+       target="_blank"
+       class="spotify-btn">
+       PLAY ON SPOTIFY
+    </a>
+
+  </div>
+  `
 ];
 
 let current = 0;
 
 function showImage(){
+
   document.getElementById("albumImage").src = images[current];
+
+  document.getElementById("slideContent").innerHTML = contents[current];
 }
 
 function nextSlide(){
@@ -21,3 +83,5 @@ function prevSlide(){
   current = (current - 1 + images.length) % images.length;
   showImage();
 }
+
+showImage();
